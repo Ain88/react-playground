40a1aef64cc2a8da054c11ae49b83de97e943729
ahyoung in <ahyungss@hotmail.com>
2022-10-24 08:55:43 -0700

    added react pro sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Sidebar>
+    <Menu>
+      <SubMenu label="Charts">
+        <MenuItem> Pie charts </MenuItem>
+        <MenuItem> Line charts </MenuItem>
+      </SubMenu>
+      <MenuItem> Documentation </MenuItem>
+      <MenuItem> Calendar </MenuItem>
+    </Menu>
+  </Sidebar>
   );
 }
 
